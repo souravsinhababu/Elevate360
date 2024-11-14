15723ad3cc9e4b5a0b5ed0bc59e0c061df0a7c85
souravsinhababu <souravsinhababu55@gmail.com>
2024-11-14 11:56:26 +0530

    created pojo classes in model

--- project/src/main/java/com/elevate360/project/model/Trainer.java
+++ project/src/main/java/com/elevate360/project/model/Trainer.java
@@ -11,4 +11,55 @@ public class Trainer {
     private String trainerUserName;
     private String trainerPassword;
     private String trainerSpecialization;
+
+    public String getTrainerEmail() {
+        return trainerEmail;
+    }
+
+    public void setTrainerEmail(String trainerEmail) {
+        this.trainerEmail = trainerEmail;
+    }
+
+    public String getTrainerUserName() {
+        return trainerUserName;
+    }
+
+    public void setTrainerUserName(String trainerUserName) {
+        this.trainerUserName = trainerUserName;
+    }
+
+    public String getTrainerPassword() {
+        return trainerPassword;
+    }
+
+    public void setTrainerPassword(String trainerPassword) {
+        this.trainerPassword = trainerPassword;
+    }
+
+    public String getTrainerSpecialization() {
+        return trainerSpecialization;
+    }
+
+    public void setTrainerSpecialization(String trainerSpecialization) {
+        this.trainerSpecialization = trainerSpecialization;
+    }
+
+    public Trainer(String trainerEmail, String trainerUserName, String trainerPassword, String trainerSpecialization) {
+        this.trainerEmail = trainerEmail;
+        this.trainerUserName = trainerUserName;
+        this.trainerPassword = trainerPassword;
+        this.trainerSpecialization = trainerSpecialization;
+    }
+
+    @Override
+    public String toString() {
+        return "Trainer{" +
+                "trainerEmail='" + trainerEmail + '\'' +
+                ", trainerUserName='" + trainerUserName + '\'' +
+                ", trainerPassword='" + trainerPassword + '\'' +
+                ", trainerSpecialization='" + trainerSpecialization + '\'' +
+                '}';
+    }
+
+    public Trainer(){}
 }
