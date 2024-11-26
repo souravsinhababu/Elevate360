4b38e0d689a490fa381ee992090a65c5cd8a1dd7
SaranE1 <saranintrepid@gmail.com>
2024-11-26 11:13:53 +0530

    admin updated

---
+++ project/src/main/java/com/elevate360/project/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.elevate360.project.controller;
+import com.elevate360.project.entity.User;
+import com.elevate360.project.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Get all trainers
+    @GetMapping("/trainers")
+    public List<User> getAllTrainers() {
+        return userRepository.findByRole("trainer");
+    }
+
+    // Get all trainees
+    @GetMapping("/trainee")
+    public List<User> getAllTrainees() {
+        return userRepository.findByRole("trainee");
+    }
+}
+
--- project/src/main/java/com/elevate360/project/repo/UserRepository.java
+++ project/src/main/java/com/elevate360/project/repo/UserRepository.java
@@ -3,7 +3,11 @@ package com.elevate360.project.repo;
 import com.elevate360.project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    List<User> findByRole(String a);
 }
 
