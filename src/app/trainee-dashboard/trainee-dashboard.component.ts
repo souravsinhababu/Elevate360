95407fbf549d16c329dd40db51b05bdf5d560cc6
Surya-1512 <suryass1215@gmail.com>
2024-11-28 11:20:40 +0530

    main branch

--- src/app/trainee-dashboard/trainee-dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-trainee-dashboard',
-  templateUrl: './trainee-dashboard.component.html',
-  styleUrls: ['./trainee-dashboard.component.css']
-})
-export class TraineeDashboardComponent {
-}
--- src/app/trainer-dashboard/trainer-dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-trainer-dashboard',
-  templateUrl: './trainer-dashboard.component.html',
-  styleUrls: ['./trainer-dashboard.component.css']
-})
-export class TrainerDashboardComponent {
-}
